28ce3c1eaa5a32d14598264ed67eaa5d87bee0c9
BalzNash <73427876+BalzNash@users.noreply.github.com>
2020-11-30 10:57:07 +0100

    encoded plots

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -66,18 +66,29 @@ def save_vocabulary(vocabulary):
     with open('vocabulary.pickle', 'wb') as g:
         pickle.dump(vocabulary, g)
 
+def encode_plot(preprocessed_plot, vocabulary, idx, cwd):
+    encoded_plots_folder = '\\encoded_files\\'
+    dict_repr = {}
+    for token in preprocessed_plot:
+        dict_repr[vocabulary[token]] = dict_repr.get(vocabulary[token], 0) + 1
+    with open(cwd+encoded_plots_folder+str(idx)+".pickle", "wb") as h:
+        pickle.dump(dict_repr, h)
 
-""" tsv_folder = '\\tsvs\\'
+tsv_folder = '\\tsvs\\'
 cwd = os.getcwd()
 vocabulary = {}
+idx = 1
 
 for file_name in os.listdir(cwd+tsv_folder):
     plot = get_plot_from_tsv(cwd+tsv_folder+file_name)
     preprocessed_plot = preprocess_text(plot)
     vocabulary = update_vocabulary(preprocessed_plot, vocabulary)
-save_vocabulary(vocabulary) """
+    encode_plot(preprocessed_plot, vocabulary, idx, cwd)
+    print(idx)
+    idx += 1
+save_vocabulary(vocabulary)
 
-file = open("vocabulary.pickle",'rb')
+file = open(cwd+"\\encoded_files\\"+str(1)+".pickle",'rb')
 object_file = pickle.load(file)
 file.close()
 print(object_file)
