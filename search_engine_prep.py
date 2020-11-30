285c3b4904ffb5dfcfb7483347f3cef9dfc5bce1
BalzNash <73427876+BalzNash@users.noreply.github.com>
2020-11-30 15:10:26 +0100

    restructured search_engine_prep

--- search_engine_prep.py
+++ search_engine_prep.py
@@ -3,28 +3,22 @@ import os
 from text_preprocessing import preprocess_text
 
 
-cwd = os.getcwd()
-encoded_files_folder = "\\encoded_files\\"
-file = open("vocabulary.pickle",'rb')
-dictionary = pickle.load(file)
-file.close()
-
-inverted_idx = {}
-for file_name in os.listdir(cwd+encoded_files_folder):
-    with open(cwd+encoded_files_folder + file_name,'rb') as f:
-        dict_repr = pickle.load(f)
-        for key in dict_repr:
-            inverted_idx.setdefault(key, []).append(int(file_name[:-7]))
+def create_inverted_idx(cwd, encoded_files_folder):
+    inverted_idx = {}
+    for file_name in os.listdir(cwd+encoded_files_folder):
+        with open(cwd+encoded_files_folder + file_name,'rb') as f:
+            dict_repr = pickle.load(f)
+            for key in dict_repr:
+                inverted_idx.setdefault(key, []).append(int(file_name[:-7]))
+    with open('inverted_idx.pickle', "wb") as g:
+        pickle.dump(inverted_idx, g)
 
 
-query = input('enter your query')
-
-preprocessed_query = preprocess_text(query)
-
-encoded_query = [dictionary[token] for token in preprocessed_query if token in dictionary]
-
-result = []
-if encoded_query:
+def search_engine(encoded_query, inverted_idx):
+    result = []
+    if not encoded_query:
+        return result
+    else:
         selected_lists = [inverted_idx[token] for token in encoded_query]
         idx = [0] * len(selected_lists)
         lists_len = [len(my_list) for my_list in selected_lists]
@@ -42,8 +36,24 @@ if encoded_query:
                     else:
                         idx[j] += 1
                     j += 1
-else:
-    pass
+        return result
 
-print(result)
+
+if __name__ == "__main__":
+    
+    cwd = os.getcwd()
+    encoded_files_folder = "\\encoded_files\\"
+    
+    create_inverted_idx(cwd, encoded_files_folder)
+    
+    with open('inverted_idx.pickle', 'rb') as h:
+        inverted_idx = pickle.load(h)
+
+    with open('vocabulary.pickle', 'rb') as q:
+        vocabulary = pickle.load(q)
+    
+    query = input('enter your query:\n')
+    preprocessed_query = preprocess_text(query)
+    encoded_query = [vocabulary[token] for token in preprocessed_query if token in vocabulary]
+    print(search_engine(encoded_query, inverted_idx))
 
