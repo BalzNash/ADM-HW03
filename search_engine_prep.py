eb0d65f36f25f80a86001c5094d97cb5b96cc1c8
BalzNash <73427876+BalzNash@users.noreply.github.com>
2020-12-01 11:29:00 +0100

    created inverted idx 2

--- search_engine_prep.py
+++ search_engine_prep.py
@@ -1,11 +1,11 @@
 import pickle
 import os
 from text_preprocessing import preprocess_text
+import math
 
 
 def create_inverted_idx(cwd, encoded_files_folder):
     inverted_idx = {}
-    #for file_name in os.listdir(cwd+encoded_files_folder):
     file_list = os.listdir(cwd+encoded_files_folder)
     file_list = sorted(file_list, key=lambda x:int(os.path.splitext(x)[0]))
     for file_name in file_list:
@@ -25,7 +25,6 @@ def encode_query(query, vocabulary):
         else:
             return False
     return encoded
-
 
 
 def search_engine(encoded_query, inverted_idx):
@@ -53,16 +52,39 @@ def search_engine(encoded_query, inverted_idx):
         return result
 
 
+def create_inverted_idx_2(cwd, encoded_files_folder):
+    inverted_idx2 = {}
+    file_list = os.listdir(cwd+encoded_files_folder)
+    file_list = sorted(file_list, key=lambda x:int(os.path.splitext(x)[0]))
+    docs_count = 0
+    for file_name in file_list:
+        docs_count += 1
+        with open(cwd+encoded_files_folder + file_name,'rb') as f:
+            dict_repr = pickle.load(f)
+            for key in dict_repr:
+                inverted_idx2.setdefault(key, []).append((int(file_name[:-7]), dict_repr[key]))
+    for key in inverted_idx2:
+        for idx, value in enumerate(inverted_idx2[key]):
+            inverted_idx2[key][idx] = (value[0], value[1] * math.log(docs_count / len(inverted_idx2[key])))
+    with open('inverted_idx2.pickle', "wb") as g:
+        pickle.dump(inverted_idx2, g)
+    pass
+
+
 if __name__ == "__main__":
     
     cwd = os.getcwd()
     encoded_files_folder = "\\encoded_files\\"
     
     create_inverted_idx(cwd, encoded_files_folder)
-    
+    create_inverted_idx_2(cwd, encoded_files_folder)
+
     with open('inverted_idx.pickle', 'rb') as h:
         inverted_idx = pickle.load(h)
-
+    print(len(inverted_idx))
+    with open('inverted_idx2.pickle', 'rb') as h:
+        inverted_idx2 = pickle.load(h)
+    print(len(inverted_idx2))
     with open('vocabulary.pickle', 'rb') as q:
         vocabulary = pickle.load(q)
 
